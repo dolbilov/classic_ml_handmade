5b022eb16cf87ab2737a68c8e431a4d5d035d91e
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2025-08-01 20:02:07 +0300

    feat!: add metrics support and move file

--- 01_linear_regression.py
+++ 01_linear_regression.py
@@ -1,19 +1,24 @@
 import numpy as np
 import pandas as pd
+from regression_metrics import RegressionMetrics
 
 
 class MyLineReg:
-    def __init__(self, n_iter: int = 100, learning_rate: float = 0.1):
+    def __init__(
+        self,
+        n_iter: int = 100,
+        learning_rate: float = 0.1,
+        metric: str | None = None
+    ):
         self.n_iter = n_iter
         self.learning_rate = learning_rate
         self.weights: np.ndarray | None = None
+        self.best_score: float | None = None
+        self.metric = metric
+        self.metric_function = RegressionMetrics.get_metric_by_name(metric) if metric else None
 
     def __repr__(self):
         return f'MyLineReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}'
-
-    @staticmethod
-    def get_mse(y_true: np.ndarray, y_pred: np.ndarray) -> float:
-        return np.square(y_true - y_pred).mean()
 
     def fit(self, X_: pd.DataFrame, y_: pd.Series, verbose: bool | int = False) -> None:
         X = X_.to_numpy()
@@ -25,11 +30,16 @@ class MyLineReg:
 
         for i in range(self.n_iter):
             y_pred = X @ self.weights
-            mse = self.get_mse(y, y_pred)
             grad = (2 / X.shape[0]) * (y_pred - y) @ X
             self.weights -= self.learning_rate * grad
+
             if verbose and (i == 0 or (i + 1) % verbose == 0):
-                print(f'[{i + 1}/{self.n_iter}] loss: {mse:.2f}')
+                mse = RegressionMetrics.mean_squared_error(y, y_pred)
+                log_text = f'[{i + 1}/{self.n_iter}] | loss = {mse:.2f}'
+                if self.metric_function is not None:
+                    metric = self.metric_function(y, y_pred)
+                    log_text += f' | {self.metric} = {metric:.2f}'
+                print(log_text)
 
     def predict(self, X_: pd.DataFrame) -> np.ndarray:
         X = X_.to_numpy()
@@ -39,3 +49,5 @@ class MyLineReg:
     def get_coef(self) -> np.ndarray:
         return self.weights[1:]
 
+    def get_best_score(self) -> float:
+        return self.best_score
---
+++ regression_metrics.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+class RegressionMetrics:
+    @staticmethod
+    def mean_absolute_error(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return np.abs(y_true - y_pred).mean()
+
+    @staticmethod
+    def mean_squared_error(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return np.square(y_true - y_pred).mean()
+
+    @staticmethod
+    def root_mean_squared_error(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return RegressionMetrics.mean_squared_error(y_true, y_pred) ** 0.5
+
+    @staticmethod
+    def r2(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        y_mean = y_true.mean()
+        return 1 - np.square(y_true - y_pred).mean() / np.square(y_true - y_mean).mean()
+
+    @staticmethod
+    def mean_absolute_percentage_error(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        n = len(y_true)
+        err = np.abs((y_true - y_pred) / y_pred).mean()
+        return 100 / n * err
+
+    @staticmethod
+    def get_metric_by_name(name: str):
+        metric_function_map = {
+            'mae': RegressionMetrics.mean_absolute_error,
+            'mse': RegressionMetrics.mean_squared_error,
+            'rmse': RegressionMetrics.root_mean_squared_error,
+            'mape': RegressionMetrics.mean_absolute_percentage_error,
+            'r2': RegressionMetrics.r2
+        }
+
+        return metric_function_map[name]
