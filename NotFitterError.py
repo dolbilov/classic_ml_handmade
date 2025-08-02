e3e7190d95dfa97eb66fa1ab4cbd0a09ebb28146
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2025-08-02 10:46:01 +0300

    fix: add default parameter value in init

--- NotFitterError.py
+++ NotFitterError.py
@@ -1,5 +1,5 @@
 class NotFittedError(Exception):
-    def __init__(self, message: str | None) -> None:
+    def __init__(self, message: str | None = None) -> None:
         self.message = message
         super().__init__(message)
 
